8cb0cde33b72de143177a08e585fa2298e98c536
Lienien <116272320+Lienien@users.noreply.github.com>
2023-10-11 09:24:22 +0200

    bing bada boom

---
+++ index.js
@@ -0,0 +1,28 @@
+//Oppgave 1
+
+var oppg1 = ["a","b","c","d","e","f"]
+console.log(oppg1[0]);
+
+//Oppgave 2
+
+var oppg2 = ["a","b","c","d","e","f"]
+console.log(oppg2[oppg2.length - 1]);
+
+//Oppgave 3
+
+var oppg3 = [3,66,45]
+
+console.log(oppg3[0]+oppg3[1]+oppg3[2]);
+console.log(oppg3[0]*oppg3[1]*oppg3[2]);
+
+//Oppgave 4
+
+const list1 = [1,2,3,4,5]
+const list2 = [0,1,2,3,4]
+const list3 = []
+
+for (let i = 0 = 0; i < list1.length; i++) {
+    list3.push(list1[i]+list2[i])
+}
+
+console.log(list3);
